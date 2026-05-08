7ae945e1b16252383c8f0860cdf6564633322343
Ayan Khandelwal <144123050+Akprogrammer-mnnit@users.noreply.github.com>
2026-05-08 16:48:56 +0530

    Log process.env.PORT value on server start to debug
    Log the value of process.env.PORT before starting the server.

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -9,6 +9,7 @@ connectDB()
     .then(() => {
         connectMongoDB()
             .then(() => {
+                console.log("Checking process.env.PORT:", process.env.PORT);
                 const PORT = process.env.PORT || 5555;
                 app.listen(PORT, "0.0.0.0", () => {
                     console.log(`🚀 Server is running on port ${PORT}`);
