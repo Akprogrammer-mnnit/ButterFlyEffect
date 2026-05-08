140e64f0ff962981bc25aad9294de0da0f7b0ba2
Ayan Khandelwal <144123050+Akprogrammer-mnnit@users.noreply.github.com>
2026-05-08 17:09:41 +0530

    Change PORT to be parsed as an integer
    Parse PORT from environment variable as an integer.

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -9,8 +9,7 @@ connectDB()
     .then(() => {
         connectMongoDB()
             .then(() => {
-                console.log("Checking process.env.PORT:", process.env.PORT);
-                const PORT = process.env.PORT || 5555;
+                const PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 5555;
                 app.listen(PORT, "0.0.0.0", () => {
                     console.log(`🚀 Server is running on port ${PORT}`);
                 });
